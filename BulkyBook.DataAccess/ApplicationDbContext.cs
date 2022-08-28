0f89ec4c58df6557cf3001729fd364cb3dddd9dd
Tushar Shinde <111012190+shindet20@users.noreply.github.com>
2022-08-28 19:36:06 +0200

    DataAccess

---
+++ BulkyBook.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
